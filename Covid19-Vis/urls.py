aa7bb50adadf13df13e7c86ef4e1b994bdff2019
wxwsunshine <49801656+wxwsunshine@users.noreply.github.com>
2020-06-10 15:09:59 +0800

    v2

--- Covid19-Vis/urls.py
+++ Covid19-Vis/urls.py
@@ -37,5 +37,6 @@ urlpatterns = [
     path('visual/getproductbar', views.getproductbar),
     path('visual/getgdpbar', views.getgdpbar),
     path('visual/getTradingdata', views.getTradingData),
+    path('visual/getLivingdata', views.getLivingData),
     path('visual/index_data', views.index_data),
 ]
