33cd0055c8b56945fccf854df0b19b32534c0f67
JiahuiWang2000 <1342415326@qq.com>
2020-06-10 12:11:37 +0800

    Modify url

--- visual/urls.py
+++ visual/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index.html', views.index, name='index'),
     path('Estate.html', views.Estate, name='Estate'),
     path('Trading.html', views.trading),
 ]
